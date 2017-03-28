61e821382f4a3c78464cb7b36c83f3a6729b9e38
victorjambo <victorjambo@live.com>
2017-03-28 21:04:16 +0300

    add stock to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324203655) do
+ActiveRecord::Schema.define(version: 20170328180048) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.decimal  "price",       precision: 8, scale: 2
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "stock",                               default: 0
   end
 
 end
