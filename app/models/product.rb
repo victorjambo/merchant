0e74860e2674b57597dd7d3c6db5913ca06f627a
victorjambo <victorjambo@live.com>
2017-04-19 22:30:40 +0300

    Category Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
 	end
 	validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 	has_many :order_items
+	belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406182420) do
+ActiveRecord::Schema.define(version: 20170419191243) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "line1"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170406182420) do
     t.string   "state"
     t.string   "zip"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -47,6 +53,8 @@ ActiveRecord::Schema.define(version: 20170406182420) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.integer  "stock",                               default: 0
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
